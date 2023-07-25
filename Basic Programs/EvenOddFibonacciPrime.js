cff145c8f06133905376b7e3e7c8411c5c2de1b1
Dhruv Karotra <Dhruv.Karotra@asl.mum.sst>
2023-07-25 09:59:15 +0530

    Assignments

--- Basic Programs/EvenOddFibonacciPrime.js
+++ Basic Programs/EvenOddFibonacciPrime.js
@@ -2,7 +2,7 @@ function isPrime(number) {
     if (number <= 1) {
       return "not a prime number";
     } 
-    for (let i = 2; i <= Math.sqrt(number); i++) {
+    for (let i = 2; i <= number/2; i++) {
       if (number % i == 0) {
         return "not a prime number";
       }
